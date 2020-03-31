4694777f901a8777880917bb8f96ef35b943ab68
wonzalez1978 <jaimegoklein@gmail.com>
2020-03-30 21:40:57 -0300

    primer commit

---
+++ app/src/main/java/cl/desafiolatam/recyclerview/MainActivity.java
@@ -0,0 +1,21 @@
+package cl.desafiolatam.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cl.desafiolatam.recyclerview.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements RestaurantesFragment.OnListFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+}
